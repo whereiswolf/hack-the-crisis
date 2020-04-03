0d3a62b1e30a5a373cfab24bfb39ee6a68b6fd3b
mwilk <michal.wilk@euvic.pl>
2020-04-03 18:46:58 +0200

    Updates

---
+++ precache-manifest.fefdf45e541df47e058104fbe0a98f5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6bb156431470e14fb4e3abb934c6500",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b3da6939b2dd59ed43c",
+    "url": "/static/js/2.d3196f52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d3196f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "410c7031bebc90e0ca1e",
+    "url": "/static/js/3.b5fb0d06.chunk.js"
+  },
+  {
+    "revision": "8fcca1428cf96ecef819",
+    "url": "/static/js/main.75d9ccb9.chunk.js"
+  },
+  {
+    "revision": "adda8711ff27cec00982",
+    "url": "/static/js/runtime-main.37bada02.js"
+  }
+]);
