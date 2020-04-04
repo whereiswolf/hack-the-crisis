71997db5507939f05a8a1ac90c1107317ad93959
iqpc.mobile.native <marcinrzepiel@wp.pl>
2020-04-04 12:02:07 +0200

    Create part of database model

---
+++ server/src/schema.ts
@@ -0,0 +1,80 @@
+import { nexusPrismaPlugin } from 'nexus-prisma'
+import { makeSchema, objectType } from 'nexus'
+
+const Voucher = objectType({
+  name: 'Voucher',
+  definition(t) {
+    t.model.id()
+    t.model.name()
+    t.model.price()
+    t.model.promotion()
+    t.model.expirationDate()
+    t.model.description()
+    t.model.imageUrl()
+  },
+})
+
+const Business = objectType({
+  name: 'Business',
+  definition(t) {
+    t.model.id()
+    t.model.name()
+    t.model.city()
+    t.model.address()
+    t.model.siteUrl()
+    t.model.amount()
+    t.model.description()
+    t.model.history()
+    t.model.imageUrl()
+    t.model.vouchers({
+      pagination: false,
+    })
+  },
+})
+
+const Query = objectType({
+  name: 'Query',
+  definition(t) {
+    t.list.field('vouchers', {
+      type: 'Voucher',
+      resolve: (_, args, ctx) => {
+        return ctx.prisma.voucher.findMany()
+      },
+    })
+    t.list.field('businesses', {
+      type: 'Business',
+      resolve: (_, args, ctx) => {
+        return ctx.prisma.business.findMany()
+      },
+    })
+  },
+})
+
+const Mutation = objectType({
+  name: 'Mutation',
+  definition(t) {
+    t.crud.createOneVoucher()
+  },
+})
+
+export const schema = makeSchema({
+  types: [Query, Mutation, Voucher, Business],
+  plugins: [nexusPrismaPlugin()],
+  outputs: {
+    schema: __dirname + '/../schema.graphql',
+    typegen: __dirname + '/generated/nexus.ts',
+  },
+  typegenAutoConfig: {
+    contextType: 'Context.Context',
+    sources: [
+      {
+        source: '@prisma/client',
+        alias: 'prisma',
+      },
+      {
+        source: require.resolve('./context'),
+        alias: 'Context',
+      },
+    ],
+  },
+})
