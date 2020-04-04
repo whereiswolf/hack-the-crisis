395a3bbf24a67a0ce70f03222d4d7ca126813955
Michał Wilk <wilk.michal@outlook.com>
2020-04-04 22:41:27 +0000

    Updates

---
+++ precache-manifest.703cd45411af6e185e21a0da276bf042.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e94e88bc0db92d058519e1a8d92fd7",
+    "url": "/hack-the-crisis/index.html"
+  },
+  {
+    "revision": "dcf8ca167e31d107dd3c",
+    "url": "/hack-the-crisis/static/js/0.b7d777cf.chunk.js"
+  },
+  {
+    "revision": "9690dcd542a3427cb472",
+    "url": "/hack-the-crisis/static/js/10.eccb16c9.chunk.js"
+  },
+  {
+    "revision": "7f367e8593b74eaa5d8b",
+    "url": "/hack-the-crisis/static/js/11.f656cda8.chunk.js"
+  },
+  {
+    "revision": "8d54be0bcdd1d9e8aa8d",
+    "url": "/hack-the-crisis/static/js/12.7cd272c7.chunk.js"
+  },
+  {
+    "revision": "da00819b53b3e434ce62",
+    "url": "/hack-the-crisis/static/js/3.bbcfce1e.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/hack-the-crisis/static/js/3.bbcfce1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b35ff8a7b64060412b",
+    "url": "/hack-the-crisis/static/js/4.b25cbf62.chunk.js"
+  },
+  {
+    "revision": "cbcd5128c942abb74ce4",
+    "url": "/hack-the-crisis/static/js/5.8d0cb38a.chunk.js"
+  },
+  {
+    "revision": "ef4699ee2689c8d76f95",
+    "url": "/hack-the-crisis/static/js/6.2fbbb28b.chunk.js"
+  },
+  {
+    "revision": "315b3ac84df40ed6ffc5",
+    "url": "/hack-the-crisis/static/js/7.d5cea8a5.chunk.js"
+  },
+  {
+    "revision": "53a5b52763ab5506d02b",
+    "url": "/hack-the-crisis/static/js/8.06cd0489.chunk.js"
+  },
+  {
+    "revision": "d19c3641ac6c6f2e19f3",
+    "url": "/hack-the-crisis/static/js/9.87970bdc.chunk.js"
+  },
+  {
+    "revision": "3eeace78c4a1ddb1b009",
+    "url": "/hack-the-crisis/static/js/main.dd5e9eed.chunk.js"
+  },
+  {
+    "revision": "eeb6eb8f1aebc8b768bf",
+    "url": "/hack-the-crisis/static/js/runtime-main.3d3f9682.js"
+  },
+  {
+    "revision": "4826d6b0b20dcc6c541d1cab4d6417bf",
+    "url": "/hack-the-crisis/static/media/dropdown.4826d6b0.svg"
+  },
+  {
+    "revision": "1a17e3f48c5c79e56de170c7301582ca",
+    "url": "/hack-the-crisis/static/media/pin.1a17e3f4.svg"
+  },
+  {
+    "revision": "9c4c546f64c365f17b7f1bbb1dcba12a",
+    "url": "/hack-the-crisis/static/media/restaurantIcon.9c4c546f.svg"
+  }
+]);
