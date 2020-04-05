51568d93dd1d5e63df6686faedecdbee82c4915a
Wojciech Bębenek <wojciech.bebenek@ac-project.net>
2020-04-05 10:09:52 +0200

    Add OrderCompleteModal component

---
+++ client/src/modules/vouchers/containers/Order/components/OrderCompleteModal/Heart.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+interface HeartProps {}
+
+const Heart = (props: HeartProps) => (
+  <svg
+    width="35"
+    height="32"
+    viewBox="0 0 35 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style={{ display: 'inline-block' }}
+    {...props}
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M16.4694 30.8156C16.7844 31.0119 17.1422 31.1111 17.5 31.1111C17.8578 31.1111 18.2156 31.0119 18.5306 30.8156C19.2033 30.3956 35 20.3992 35 9.72222C35 4.36139 30.6386 0 25.2778 0C22.1783 0 19.3064 1.6275 17.5 4.04444C15.6936 1.6275 12.8217 0 9.72222 0C4.36139 0 0 4.36139 0 9.72222C0 20.3992 15.7967 30.3956 16.4694 30.8156Z"
+      fill="#27C9B0"
+    />
+  </svg>
+)
+
+export default Heart
