89cc036b7a3e6c1380195f213658300b91e1eab8
Michał Wilk <wilk.michal@outlook.com>
2020-04-05 14:36:15 +0000

    Updates

---
+++ precache-manifest.2b5a9538ea5cb5e8ba1642a07dce26bc.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69fddadae11bbe2bfbdadca71330e16",
+    "url": "/hack-the-crisis/index.html"
+  },
+  {
+    "revision": "cab92b15d0a04cf7f316",
+    "url": "/hack-the-crisis/static/js/0.32226148.chunk.js"
+  },
+  {
+    "revision": "bbcdec4eeca76ebe587e",
+    "url": "/hack-the-crisis/static/js/10.45dff353.chunk.js"
+  },
+  {
+    "revision": "0773f124087567791313",
+    "url": "/hack-the-crisis/static/js/11.4d9bc6e1.chunk.js"
+  },
+  {
+    "revision": "2c5fbf55e8c0ccba26da",
+    "url": "/hack-the-crisis/static/js/12.c4e80606.chunk.js"
+  },
+  {
+    "revision": "e6ac581278e0edf27a2b",
+    "url": "/hack-the-crisis/static/js/13.45ea9722.chunk.js"
+  },
+  {
+    "revision": "79aec2eee51cb820d768",
+    "url": "/hack-the-crisis/static/js/3.3f47a7bd.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/hack-the-crisis/static/js/3.3f47a7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a0c0e831f6f17d29e8",
+    "url": "/hack-the-crisis/static/js/4.adf6aaac.chunk.js"
+  },
+  {
+    "revision": "ea0bce3b9c1141e047a7e57907711738",
+    "url": "/hack-the-crisis/static/js/4.adf6aaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f870044cbe32acfdfcbf",
+    "url": "/hack-the-crisis/static/js/5.c2f69290.chunk.js"
+  },
+  {
+    "revision": "c2686aef956b66214c76",
+    "url": "/hack-the-crisis/static/js/6.3e3e79a8.chunk.js"
+  },
+  {
+    "revision": "b539d5c8870afa36e7e8",
+    "url": "/hack-the-crisis/static/js/7.4470ed3b.chunk.js"
+  },
+  {
+    "revision": "6da96177461db5e62d40",
+    "url": "/hack-the-crisis/static/js/8.57b8a450.chunk.js"
+  },
+  {
+    "revision": "b02c6543f37f69c2d929",
+    "url": "/hack-the-crisis/static/js/9.71d9d976.chunk.js"
+  },
+  {
+    "revision": "78b1720626eb2bc23c6c",
+    "url": "/hack-the-crisis/static/js/main.f9a16f3e.chunk.js"
+  },
+  {
+    "revision": "e129ff2634b64ec17a37",
+    "url": "/hack-the-crisis/static/js/runtime-main.c61c38a4.js"
+  },
+  {
+    "revision": "74cd1b40c6680d2f78d1f5f3d67b2422",
+    "url": "/hack-the-crisis/static/media/background.74cd1b40.png"
+  },
+  {
+    "revision": "712d331310f2d3ee2947c1b845b81561",
+    "url": "/hack-the-crisis/static/media/demo.712d3313.png"
+  },
+  {
+    "revision": "4826d6b0b20dcc6c541d1cab4d6417bf",
+    "url": "/hack-the-crisis/static/media/dropdown.4826d6b0.svg"
+  },
+  {
+    "revision": "8b757db663b1d45f82cbe96c9ae04986",
+    "url": "/hack-the-crisis/static/media/login-bg.8b757db6.png"
+  },
+  {
+    "revision": "1a17e3f48c5c79e56de170c7301582ca",
+    "url": "/hack-the-crisis/static/media/pin.1a17e3f4.svg"
+  },
+  {
+    "revision": "9c4c546f64c365f17b7f1bbb1dcba12a",
+    "url": "/hack-the-crisis/static/media/restaurantIcon.9c4c546f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hack-the-crisis/precache-manifest.679991ebd37876ae13f595bc6d7feffe.js"
+  "/hack-the-crisis/precache-manifest.2b5a9538ea5cb5e8ba1642a07dce26bc.js"
 );
 
 self.addEventListener('message', (event) => {
